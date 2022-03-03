5bd75d62dd0846e1e0481bd9916daf2a2c8edb9e
habeeb063 <habeebmohammed6666@gmail.com>
2022-03-03 19:55:32 +0530

    commit

--- 3_Implementation/inc/Ebill.h
+++ 3_Implementation/inc/Ebill.h
@@ -1,11 +1,33 @@
 #ifndef __Ebill_H__
 #define __Ebill_H__
-
+/**
+ * @file Ebill.h
+ * @author K MOHAMMAD HABEEB (habeebmohammed@6666gmail.com)
+ * @brief Calculation of Electricity Bill
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include<stdio.h>
 #include<stdlib.h>
-
 #include<string.h>
 
+/**
+ * @brief Structure of Amount 
+ * 
+ */
+struct Amount
+{
+	int amount ;
+};
+
+struct Amount A;
+/**
+ * @brief Structure of Bill
+ * 
+ */
 struct Bill
 {
 	
@@ -15,16 +37,22 @@ struct Bill
 	char email[20];
 	int phonenumber[11];
 };
-struct Amount
-{
-	int amount ;
-};
 
-struct Amount A;
 struct Bill B;
-
+/**
+ * @brief Records the details of user
+ * 
+ */
 void details();
+/**
+ * @brief Calculates the bill 
+ * 
+ */
 void Bill();
+/**
+ * @brief Displays the bill and other dtails of user
+ * 
+ */
 void display ();
 
 #endif
