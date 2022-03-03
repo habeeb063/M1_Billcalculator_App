c4992c82cb99573d506343d3590e9310c9cdc6aa
habeeb063 <90189001+habeeb063@users.noreply.github.com>
2022-03-03 20:15:03 +0530

    commit

--- 3_Implementation/inc/Ebill.h
+++ 3_Implementation/inc/Ebill.h
@@ -1,40 +1,22 @@
 
-/**
- * @file Ebill.h
- * @author K Mohammad Habeeb (habeebmohammed6666@gmail.com)
- * @brief Calculates the Electricity Bill
- * @version 0.1
- * @date 2022-03-03
- * 
- * @copyright Copyright (c) 2022
- * 
- */
- #ifndef __Ebill_H__
+
+#ifndef __Ebill_H__
 #define __Ebill_H__
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-/**
- * @brief Stores the amount
- * 
- */
+
 struct Amount
 {
 	int amount ;
 };
 
-/**
- * @brief Stores the amount
- * 
- */
+
 
 struct Amount A;
-/**
- * @brief Stores the amount
- * 
- */
+
 
 struct Bill
 {
@@ -46,21 +28,11 @@ struct Bill
 };
 
 struct Bill B;
-/**
- * @brief Stores the amount
- * 
- */
+
 void details();
-/**
- * @brief Stores the amount
- * 
- */
 
 void Bill();
-/**
- * @brief Stores the amount
- * 
- */
+
 void display ();
 
 #endif
